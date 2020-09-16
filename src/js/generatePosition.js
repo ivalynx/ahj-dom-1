4540803c81bdc0c9ea6aa6a9f7210be870549796
Iva Kochneva <inkybasik@gmail.com>
2020-09-16 20:03:35 +0300

    Соберись, тряпка 3

--- src/js/generatePosition.js
+++ src/js/generatePosition.js
@@ -1,6 +1,6 @@
 export default function generatePosition(length, previousPosition) {
-  let result = Math.floor(Math.random() * length);
-  if(result === previousPosition) {
+  const result = Math.floor(Math.random() * length);
+  if (result === previousPosition) {
     generatePosition(length, previousPosition);
   }
   return result;
